acf3e7836d8da5146db9a370ab262b66f530bc6c
Callum Messiter <cmessiter@hotmail.co.uk>
2017-10-24 14:12:33 +0200

    Compose and embed HTML templates

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,121 +4,9 @@
     <!-- Notification alerts for item/category updates/deletions --> 
     <alert :showAlert="alert"></alert>
     
-    <!-- START: CategoriesAccordion Component -->
-    <div class="panel-group" id="accordion">
-      <div v-for="category in categories" class="panel panel-default">
-        <div class="panel-heading">
-          <h4 class="panel-title">
-            <a data-toggle="collapse" data-parent="#accordion" v-bind:href="'#' + category.categoryId">
-            {{category.name}}</a>
-          </h4>
-        </div>
-        <div 
-          class="panel-collapse collapse"
-          v-bind:id="category.categoryId"  
-          v-bind:class="{'in': categories.indexOf(category) == 0}"
-        >
-          <div class="panel-body">
-
-            <!-- START: Item Component -->
-            <table class="table table-bordered">
-              <thead class="thead-default">
-                <tr>
-                    <th class="text-center">Name</th>
-                    <th class="text-center">Price</th>
-                    <th class="text-center">Description</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="item-row" v-for="item in category.items">
-                    <td class="item-name text-left col-md-2">
-                      <input 
-                        type="text" 
-                        class="form-control" 
-                        v-model="item.name" 
-                        v-bind:readonly="editableItem.id != item.itemId" 
-                        v-on:dblclick="
-                          makeItemEditable(
-                            item.itemId, 
-                            category.items.indexOf(item), 
-                            category.categoryId, 
-                            categories.indexOf(category)
-                          )"
-                      > 
-                    </td>
-                    <td class="item-price text-left col-md-1">
-                      <input 
-                        type="text" 
-                        class="form-control" 
-                        v-model="item.price" 
-                        v-bind:readonly="editableItem.id != item.itemId" 
-                        v-on:dblclick="
-                          makeItemEditable(
-                            item.itemId, 
-                            category.items.indexOf(item), 
-                            category.categoryId, 
-                            categories.indexOf(category)
-                          )"
-                      >
-                    </td>
-                    <td class="item-description text-left col-md-5">
-                      <input 
-                        type="text" 
-                        class="form-control" 
-                        v-model="item.description" 
-                        v-bind:readonly="editableItem.id != item.itemId" 
-                        v-on:dblclick="
-                          makeItemEditable(
-                            item.itemId, 
-                            category.items.indexOf(item), 
-                            category.categoryId, 
-                            categories.indexOf(category)
-                          )"
-                      >
-                    </td>
-                    <td class="buttons col-md-2">
-                      <button 
-                        class="btn btn-danger pull-left align-middle"
-                        v-if="editableItem.id != item.itemId"
-                        v-on:click="
-                          showConfirmDeleteModal(
-                            item.itemId, 
-                            category.items.indexOf(item), 
-                            categories.indexOf(category)
-                          )"
-                        >Delete
-                      </button>
-                      <button 
-                        class="btn btn-primary pull-left align-middle"
-                        v-if="editableItem.id == item.itemId" 
-                        v-on:click="
-                          showConfirmUpdateModal(
-                            item, 
-                            category.items.indexOf(item), 
-                            categories.indexOf(category)
-                          )"
-                        >Save</button>
-                      <button 
-                        class="btn btn-danger pull-left align-middle cancelBtn"
-                        v-if="editableItem.id == item.itemId" 
-                        v-on:click="
-                          showConfirmDiscardModal(
-                            item, 
-                            category.items.indexOf(item), 
-                            categories.indexOf(category)
-                          )"
-                        >Cancel</button>
-                    </td>
-                </tr>
-              </tbody>
-            </table>
-            <!-- END: Item Component -->
-
-          </div>
-        </div>
-      </div>
+    <div class="categories-accordion">
+      <category :renderCategories="categories"></category>
     </div>
-    <!-- END: CategoriesAccordion Component -->
 
     <!-- Confirmation modals triggered when the user initialises an "update" or "deletion" -->
     <modal
@@ -138,12 +26,14 @@ import cloneDeep from 'clone-deep';
 import lodash from 'lodash';
 
 // Components
+import Category from './Category';
 import Modal from './Modal';
 import Alert from './Alert';
 
 export default {
   name: 'Menu',
   components: {
+    'category': Category,
     'modal': Modal,
     'alert': Alert
   },
@@ -377,74 +267,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  /** Inputs should fill the entire cell **/
-
-  td {
-    padding: 0 !important;
-  }
-  /** Inputs should  have no border **/
-  input {
-    border: 0 !important;
-  }
-  
-  input[readonly]{
-    background-color: #f5f5f0;
-    /** Readonly inputs should not glow upon click **/
-    outline: none;
-    border: none !important;
-    -webkit-box-shadow: none !important;
-    -moz-box-shadow: none !important;
-    box-shadow: none !important;
-  }
-
-  button {
-    height: 32px;
-    width: 67px;
-    margin-left: 3px;
-    margin-top: 1px;
-  }
-
-  .cancelBtn {
-    background-color: #404040
-  }
-
-  .cancelBtn:hover {
-    background-color: #1a1a1a;
-  }
-
-  .cancelBtn:active {
-    background-color: #000000
-  }
-
-  table {
-    border: 0 !important;
-    margin-bottom: 0 !important;
-  }
-
-  .buttons {
-    border: none !important;
-  }
-
-  .accordion {
-    border: 0 !important;
-    padding: 0 !important;
-    margin: 0 !important;
-  }
-
-  .panel {
-    border: none !important;
-  }
-
-  .panel-body {
-    padding: 0 !important;
-    margin: 0 !important;
-    border: 0 !important;
-    border-bottom: 0 !important;
-  }
-
-  .panel-heading {
-    background-color: #8d8d8e !important;
-    color: white !important;
-  }
-
 </style>
