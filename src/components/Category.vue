309c53ca2cb96d531ded4de9ac012aa10ef8d5f3
Callum Messiter <cmessiter@hotmail.co.uk>
2017-11-04 18:48:26 +0100

    Add basis of category-modification functionality

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -3,14 +3,54 @@
     <div v-for="category in categories" class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
+          <input 
+            v-if="editMode.active" 
+            class="categoryName" 
+            v-model="category.name"
+            :class="{editMode: editMode.active && editMode.category.id == category.categoryId}"
+            :readonly="editMode.category.id != category.categoryId">
+          </input>
           <a 
+            v-if="!editMode.active"
             data-toggle="collapse" 
             data-parent="#accordion" 
-            v-bind:href="'#' + category.categoryId">
-            {{category.name}}
+            v-bind:href="'#' + category.categoryId"
+            >{{category.name}}
           </a>
-          <a href="#" v-on:click="deleteCategory(categories.indexOf(category), category.name, category.items.length)">
+          <!-- Delete Icon (visible by default) -->
+          <a 
+            href="#" 
+            v-if="!editMode.active || editMode.category.id != category.categoryId"
+            v-on:click="deleteCategory(
+              categories.indexOf(category), 
+              category.name, 
+              category.items.length
+            )">
             <span class="glyphicon glyphicon-trash pull-right align-middle"></span>
+          </a>
+          <!-- Edit Icon (visible by default) -->
+          <a 
+            href="#"
+            v-if="!editMode.active || editMode.category.id != category.categoryId" 
+            v-on:click="activateEditMode(
+              category.categoryId, 
+              categories.indexOf(category)
+            )">
+            <span class="glyphicon glyphicon-pencil pull-right align-middle"></span>
+          </a>
+          <!-- Discard Icon (visible only when a category name is being edited -->
+          <a 
+            href="#" 
+            v-if="editMode.active && editMode.category.id == category.categoryId"
+            v-on:click="discardChanges()">
+            <span class="glyphicon glyphicon-repeat pull-right align-middle"></span>
+          </a>
+          <!-- Save Icon (visible only when a category name is being edited -->
+          <a 
+            href="#" 
+            v-if="editMode.active && editMode.category.id == category.categoryId"
+            v-on:click="updateCategoryName()">
+            <span class="glyphicon glyphicon-floppy-disk pull-right align-middle"></span>
           </a>
         </h4>
       </div>
@@ -45,6 +85,8 @@ import Item from './Item';
 
 import { bus } from '../main';
 
+import lodash from 'lodash';
+
 export default {
   name: 'Category',
   components: {
@@ -53,6 +95,13 @@ export default {
 
   data() {
     return {
+      editMode: {
+        active: false,
+        category: {
+          id: '',
+          index: ''
+        }
+      }
     }
   },
 
@@ -61,12 +110,52 @@ export default {
       return this.$store.getters.getCategoriesAndItemsView;
     },
 
+    categoriesState () {
+      return this.$store.getters.getCategoriesAndItems;
+    },
+
     restaurantId () {
       return this.$store.getters.getRestaurant.restaurantId;
     }
   },
 
   methods: {
+    activateEditMode(categoryId, index) {
+      this.editMode.active = true;
+      this.editMode.category.id = categoryId;
+      this.editMode.category.index = index;
+      this.accordion = '';
+    },
+
+    updateCategoryName() {
+      // Check first if it has departed from its state
+      console.log(this.categories[this.editMode.category.index].name);
+      // API call
+
+      // If successful update the state, and show a success alert
+      //this.$store.commit('updateCategoryName',)
+      // Exit edit mode
+      this.exitEditMode();
+    },
+
+    discardChanges() {
+      // Check if the category name has departed from its state
+      if(!_.isEqual(this.categories, this.categoriesState)) {
+        // Show a warning
+        alert('Are you sure you want to discard your changes?');
+        // Set the category back to its pre-edit state
+        // On Confirmation: this.categories[this.editMode.category.index].name = this.categoriesState[this.editMode.category.index].name;
+      } 
+      // Exit edit mode
+      this.exitEditMode();
+    },
+
+    exitEditMode() {
+      this.editMode.active = false;
+      this.editMode.category.id = '';
+      this.editMode.category.index = '';
+    },
+
     deleteCategory(catIndex, catName, numItems) {
       // If the category has no items, just delete it without showing a warning
       if(numItems < 1) {
@@ -119,6 +208,10 @@ export default {
     color: white !important;
   }
 
+  .panel-title {
+    height: 17.6px;
+  }
+
   table {
     border: 0 !important;
     margin-bottom: 0 !important;
@@ -128,8 +221,31 @@ export default {
     left: 5px;
   }
 
-  .glyphicon-pencil {
+  .glyphicon-repeat {
+    left: 5px;
+  }
+
+  .glyphicon-pencil .glyphicon-floppy-disk {
     right: 5px;
+  }
+
+  .categoryName {
+    background: none;
+    border: none;
+    text-align: center
+  }
+
+  .categoryName:focus {
+    outline: none;
+    -webkit-box-shadow: none !important;
+    -moz-box-shadow: none !important;
+    box-shadow: none !important;
+  }
+
+  .editMode {
+    border-bottom: solid 2px white !important;
+    display: inline;
+    padding-bottom: 4px;
   }
 
 </style>
