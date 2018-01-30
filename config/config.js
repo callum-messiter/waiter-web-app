74b2b3f7d61a7e12077189acf372b78f8d0079f6
Callum Messiter <cmessiter@hotmail.co.uk>
2018-01-30 10:42:02 +0000

    Fix baseUrl, merge scroller

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-let baseUrl = ''
+var baseUrl = ''
 
 if(process.env.NODE_ENV === 'production') {
-   baseUrl = 'http://callummessiter.work/api';
+   baseUrl = 'http://callummessiter.work:3000/api';
 } else if(process.env.NODE_ENV === 'development') {
-   baseUrl = 'http://callummessiter.work/api';
+   baseUrl = 'http://callummessiter.work:3000/api';
 }
 
 module.exports.apiBaseUrl = baseUrl;
